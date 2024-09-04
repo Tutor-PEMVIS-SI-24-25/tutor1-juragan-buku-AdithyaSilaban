4a79581fe176271d65d59c6979216f34a8643d57
AdithyaSilaban <adithyasilaban@gmail.com>
2024-09-04 21:27:16 +0700

    Telah menyelesaikan tugas

--- T01.java
+++ T01.java
@@ -1,3 +1,18 @@
+// 12S24029 - ADITHYA SILABAN
 // NIM - NAMA
-// NIM - NAMA
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String nama;
+
+        nama = input.nextLine();
+        System.out.println(Adithya);
+    }
+}
+
+
 
